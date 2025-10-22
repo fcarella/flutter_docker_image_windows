0edd13aaa763b2a04e4000a0a1955e78736c6be6
Fred Carella <fred.carella@gmail.com>
2025-10-22 06:53:06 -0400

    commit

--- Update-permissions.ps1
+++ Update-permissions.ps1
@@ -1,2 +1,3 @@
-Write-Host "run as administrator"
-Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process -Force
+Write-Host "will not run as a script"
+Write-Host "copy and paste this at the command line"
+Write-Host "Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process -Force"
